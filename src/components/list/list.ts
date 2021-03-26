9b37ff9f45d381bcb6a55b4f8f60d1f0d640f8bb
Joy Lee <joylee.dev@gmail.com>
2021-03-27 00:17:17 +0900

    2.기능구현 :  리스트 삭제버튼

--- src/components/list/list.ts
+++ src/components/list/list.ts
@@ -1,6 +1,9 @@
 import { BaseComponent, Component } from '../component.js';
 
+type OnCloseListener = () => void;
+
 export class ListItem extends BaseComponent<HTMLElement> {
+  private closeListener?: OnCloseListener; // 외부에서 전달받은 콜백함수
   constructor() {
     super(`
       <section class="list-item">
@@ -10,13 +13,24 @@ export class ListItem extends BaseComponent<HTMLElement> {
         </div>
       </section>
     `);
+
+    const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener(); // 클릭 시 콜백함수 실행
+    };
   }
 
+  // 전달받은 item을 html에 추가
   addChild(child: Component) {
     const container = this.element.querySelector(
       '.list-item__body'
     )! as HTMLElement;
     child.attachTo(container);
+  }
+
+  // 전달받은 콜백함수를 멤버변수에 저장
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
   }
 }
 
@@ -29,5 +43,9 @@ export class List extends BaseComponent<HTMLDivElement> {
     const item = new ListItem();
     item.addChild(section);
     item.attachTo(this.element, 'beforeend');
+    item.setOnCloseListener(() => {
+      // close버튼 클릭 시 실행할 콜백함수 정의
+      item.removeFrom(this.element);
+    });
   }
 }
