910294cc682384204d3741d1fdacff6f2c670f01
Joy Lee <joylee.dev@gmail.com>
2021-03-27 16:54:55 +0900

    2.기능구현 : 다이얼로그 화면 추가, 이벤트함수 정의

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,7 @@
 import { List } from './components/list/list.js';
 import { ImageComponent } from './components/list/item/image.js';
 import { NoteComponent } from './components/list/item/note.js';
+import { Dialog } from './components/dialog/dialog.js';
 
 class App {
   private readonly list: List;
@@ -16,6 +17,21 @@ class App {
 
     const note = new NoteComponent('Note Title', 'hello');
     this.list.addChild(note);
+
+    const noteBtn = document.querySelector('#new-note')! as HTMLButtonElement;
+    noteBtn.addEventListener('click', () => {
+      const dialog = new Dialog();
+      dialog.attachTo(document.body);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(document.body);
+      });
+
+      dialog.setOnSubmitListener(() => {
+        // 섹션을 만들어 페이지에 추가한다.
+        dialog.removeFrom(document.body);
+      });
+    });
   }
 }
 
---
+++ src/components/dialog/dialog.ts
@@ -0,0 +1,47 @@
+import { BaseComponent } from '../component.js';
+
+type OnCloseListener = () => void;
+type OnSubmitListener = () => void;
+
+export class Dialog extends BaseComponent<HTMLElement> {
+  // 외부에서 전달받은 Event Listener 저장 장소
+  closeListener?: OnCloseListener;
+  submitListener?: OnSubmitListener;
+
+  constructor() {
+    super(`
+      <div class="dialog">
+        <div class="dialog__container">
+          <button class="close">&times;</button>
+          <div class="dialog__body"></div>
+          <button class="submit">ADD</button>
+        </div>
+      </div>
+    `);
+
+    // 👉 이벤트 처리 방법
+    // 👉 내부적으로 처리하지 않고 외부로 부터 전달 받은 리스너가 있으면 호출해 주는 방식으로 작성한다.
+
+    const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
+
+    const submitBtn = this.element.querySelector(
+      '.submit'
+    )! as HTMLButtonElement;
+    submitBtn.onclick = () => {
+      this.submitListener && this.submitListener();
+    };
+  }
+
+  // 외부에서 이벤트 전달에 사용할 methods.
+  // 전달받은 Event Listener를 각각의 내부 변수에 저장한다.
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
+  }
+
+  setOnSubmitListener(listener: OnSubmitListener) {
+    this.submitListener = listener;
+  }
+}
